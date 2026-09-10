3c1f0dc15498ca1b9643ecaee5f1ba51e206e5cf
cod-md <92212094+cod-md@users.noreply.github.com>
2026-09-10 14:16:21 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,9 +39,16 @@ android {
     }
 }
 
-dependencies { implementation("androidx.recyclerview:recyclerview:1.3.2") }
-
 dependencies {
-    compileOnly("androidx.annotation:annotation:1.9.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+
+    implementation("androidx.appcompat:appcompat:1.8.0")
+
+    // Needed by MainActivity to communicate with LSPosed
+    implementation("io.github.libxposed:service:102.0.0")
+
+    // Xposed API used by your module
     compileOnly("io.github.libxposed:api:102.0.0")
+
+    compileOnly("androidx.annotation:annotation:1.9.1")
 }
